1c603f7d25b376e06af3439e6458dc24d8eb477c
Rafael Cabrera <rafael.m.cabrera.1@gmail.com>
2025-02-09 00:46:32 +0100

    groq setup

---
+++ health_automation/config/groq_setup.py
@@ -0,0 +1,11 @@
+import os
+import json
+
+# Carregar credenciais do Groq
+def load_groq_credentials():
+    certs_path = os.path.join("certs", "groq.json")
+    with open(certs_path, "r") as file:
+        return json.load(file)
+
+groq_creds = load_groq_credentials()
+os.environ["GROQ_API_KEY"] = groq_creds["GROQ_API_KEY"]  # Define a chave no ambiente
